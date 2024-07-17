f080d225d2a596bd000d101d37c06dd8975a5e86
qkenn <github.com.sampling990@passinbox.com>
2024-07-17 16:56:32 +0530

    feat: change grid size based on user input

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 const gridContainer = document.getElementById('grid-container');
-const divs = document.querySelector('.item');
 
-for (let i = 0; i < 50 * 50; i++) {
-  const gridItem = document.createElement('div');
-  gridItem.className = 'item';
-  gridContainer.appendChild(gridItem);
+function getGridSize() {
+  const gridSize = window.prompt('Enter grid size', 30);
+
+  if (isNaN(+gridSize) && !gridSize) return;
+
+  createGrid(gridSize);
 }
 
-gridContainer.addEventListener('mouseover', (e) => {
-  e.target.style.backgroundColor = 'black';
-});
+function createGrid(gridSize) {
+  if (!gridSize) return;
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const gridItem = document.createElement('div');
+    gridItem.className = 'item';
+
+    gridItem.style.width = `${800 / gridSize - 2}px`;
+    gridItem.style.heigh = `${800 / gridSize - 2}px`;
+
+    gridContainer.appendChild(gridItem);
+  }
+
+  changeGridColor();
+}
+
+function changeGridColor() {
+  gridContainer.addEventListener('mouseover', (e) => {
+    e.target.style.backgroundColor = 'black';
+  });
+}
+
+getGridSize();
